0465f30b4e49d28b70b22b8a57623eb0b8dddadc
Niklas <stangl@protonmail.com>
2023-02-02 11:40:17 +0100

    added simple client example

---
+++ python-client/swagger_client/api/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from swagger_client.api.contexts_api import ContextsApi
+from swagger_client.api.graph_store_api import GraphStoreApi
+from swagger_client.api.namespaces_api import NamespacesApi
+from swagger_client.api.protocol_api import ProtocolApi
+from swagger_client.api.repositories_api import RepositoriesApi
+from swagger_client.api.sparql_api import SparqlApi
+from swagger_client.api.transactions_api import TransactionsApi
